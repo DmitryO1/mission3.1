595b0a22beb56000a690109ac9eeca35cab7439e
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-08 19:28:26 +0300

    fafa

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Dmitry on 08.11.2016.
+ */
+public class Calculator {
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int divide(int a, int b){
+
+        return a/b;
+    }
+
+    public int subtr(int a, int b){
+        return a-b;
+    }
+
+
+}
